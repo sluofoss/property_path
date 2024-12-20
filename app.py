560d7d140b6e1fb5f1b13fbf0b42bdaa15c4c782
sluo <41530060+sluofoss@users.noreply.github.com>
2024-12-20 17:29:26 +1100

    last working state

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
+from datatableio import DataTableIO
 import pandas as pd
 import plotly.express as px
 
@@ -20,27 +21,6 @@ prop_col = [
 ]
 loan_col = ["bank", "loan name", "rate", "repay interest only", "year period"]
 
-
-property_table = dash_table.DataTable(
-    id="property info table",
-    columns=([{"id": c, "name": c} for c in prop_col]),
-    data=[dict(Model=i, **{param: None for param in prop_col}) for i in range(1, 5)],
-    editable=True,
-    row_deletable=True,
-    export_format="csv",
-    export_headers="display",
-    # style={'width': '45%', 'display': 'inline-block', 'margin': '10px'}
-)
-loan_table = dash_table.DataTable(
-    id="home loan info table",
-    columns=([{"id": c, "name": c} for c in loan_col]),
-    data=[dict(Model=i, **{param: None for param in loan_col}) for i in range(1, 5)],
-    editable=True,
-    row_deletable=True,
-    export_format="csv",
-    export_headers="display",
-    # style={'width': '45%', 'display': 'inline-block', 'margin': '10px'}
-)
 app.layout = [
     html.H1(
         children="Investment and principal residence return calculator",
@@ -53,36 +33,8 @@ app.layout = [
     ),
     html.Div(
         [
-            html.Div(
-                [
-                    html.Div(
-                        [
-                            # html.H2('file upload', style={'margin':'10px'}),
-                            # html.H2('file download', style={'margin':'10px'}),
-                            dcc.Upload(
-                                id="datatable-upload-" + tid,
-                                children=html.Div(
-                                    ["Drag and Drop or ", html.A("Select Files")]
-                                ),
-                                style={
-                                    "width": "100%",
-                                    "height": "60px",
-                                    "lineHeight": "60px",
-                                    "borderWidth": "1px",
-                                    "borderStyle": "dashed",
-                                    "borderRadius": "5px",
-                                    "textAlign": "center",
-                                    "margin": "10px",
-                                },
-                            )
-                        ],
-                        style={"display": "flex"},
-                    ),
-                    html.Div(table, style={"display": "block"}),
-                ],
-                style={"width": "40%", "display": "inline-block", "margin": "10px"},
-            )
-            for tid, table in [("property", property_table), ("loan", loan_table)]
+            DataTableIO(prop_col, 'property'),
+            DataTableIO(loan_col, 'loan'),
         ]
     ),
     html.Hr(),
--- datatableio.py
+++ datatableio.py
@@ -1,6 +1,9 @@
 from dash import Dash, Output, Input, State, html, dcc, callback, MATCH, dash_table
 import uuid
+
 import pandas as pd
+import base64
+import io
 
 
 class DataTableIO(html.Div):
@@ -12,16 +15,31 @@ class DataTableIO(html.Div):
             "subcomponent": "datatable",
             "aio_id": aio_id,
         }
-        store = lambda aio_id: {
+        upload = lambda aio_id: {
             "component": "DataTableAIO",
-            "subcomponent": "store",
+            "subcomponent": "upload",
+            "aio_id": aio_id,
+        }
+        download_button = lambda aio_id: {
+            "component": "DataTableAIO",
+            "subcomponent": "download_button",
+            "aio_id": aio_id,
+        }
+        download = lambda aio_id: {
+            "component": "DataTableAIO",
+            "subcomponent": "download",
+            "aio_id": aio_id,
+        }
+        addrow = lambda aio_id: {
+            "component": "DataTableAIO",
+            "subcomponent": "addrow",
             "aio_id": aio_id,
         }
 
     # Make the ids class a public class
     ids = ids
 
-    def __init__(self, df: pd.DataFrame = None, aio_id=None, **datatable_props):
+    def __init__(self, columns: list = [], aio_id=None, **datatable_props):
         """DataTableIO is an All-in-One component that is composed of a parent `html.Div`
         with a `dcc.Store` and a `dash_table.DataTable` as children.
 
@@ -34,45 +52,125 @@ class DataTableIO(html.Div):
 
         # Define the component's layout
         super().__init__(
-            [
-                # Equivalent to `html.Div([...])`
-                # html.H2('file upload', style={'margin':'10px'}),
-                # html.H2('file download', style={'margin':'10px'}),
+            [# [] Equivalent to `html.Div([...])`
+
                 html.Div(
                     [
                         dcc.Upload(
-                            id=aio_id + "datatable-upload",
+                            id=self.ids.upload(aio_id),
                             children=html.Div(
-                                ["Drag and Drop or ", html.A("Select Files")]
+                                ["Drag and Drop or ", html.A("Select Files")],
+                                style = {
+                                    'margin':'10px'
+                                }
                             ),
                             style={
-                                "width": "100%",
-                                "height": "60px",
-                                "lineHeight": "60px",
+                                "height": "40px",
+                                "lineHeight": "10px",
                                 "borderWidth": "1px",
                                 "borderStyle": "dashed",
                                 "borderRadius": "5px",
                                 "textAlign": "center",
                                 "margin": "10px",
                             },
-                        )
+                        ),
+                        html.Button(
+                            "Download",
+                            id=self.ids.download_button(aio_id),
+                            n_clicks=0,
+                            style={
+                                "height": "40px",
+                                "lineHeight": "10px",
+                                "borderWidth": "1px",
+                                "borderRadius": "5px",
+                                "textAlign": "center",
+                                "margin": "10px",
+                            },
+                        ),
+                        dcc.Download(id=self.ids.download(aio_id)),
+                        html.Button(
+                            "Add row",
+                            id=self.ids.addrow(aio_id),
+                            n_clicks=0,
+                            style={
+                                "height": "40px",
+                                "lineHeight": "10px",
+                                "borderWidth": "1px",
+                                "borderRadius": "5px",
+                                "textAlign": "center",
+                                "margin": "10px",
+                            },
+                        ),
                     ],
                     style={"display": "flex"},
                 ),
                 html.Div(
                     dash_table.DataTable(
-                        id=aio_id + "datatable",
-                        columns=([{"id": c, "name": c} for c in df.columns]),
+                        id=self.ids.datatable(aio_id),
+                        columns=([{"id": c, "name": c} for c in columns]),
                         data=[
-                            dict(Model=i, **{param: None for param in df.columns})
+                            dict(Model=i, **{param: None for param in columns})
                             for i in range(1, 5)
                         ],
                         editable=True,
                         row_deletable=True,
-                        export_format="csv",
-                        export_headers="display",
+                        #export_format="csv",
+                        #export_headers="display",
                     ),
                     style={"display": "block"},
                 ),
-            ]
+            ], 
+            #style = {'display':'grid', 'grid-template-columns': 'auto auto auto', 'width':'100px'})
+            style = {"width": "40%", "display": "inline-block", "margin": "10px"}
         )
+
+    #@callback(
+    #    Output(ids.datatable(MATCH), 'data'),
+    #    Output(ids.datatable(MATCH), 'columns'),
+    #    Input(ids.upload(MATCH), 'contents'),
+    #    State(ids.upload(MATCH), 'filename'))
+    #def upload_file(contents, filename):
+    #    if contents is None:
+    #        return [{}], []
+    #    df = parse_contents(contents, filename)
+    #    return df.to_dict('records'), [{"name": i, "id": i} for i in df.columns]
+
+
+    #@callback(
+    #    Output(ids.download(MATCH), "data"),
+    #    Input(ids.download_button(MATCH), "n_clicks"),
+    #    State(ids.datatable(MATCH),"data"),
+    #    prevent_initial_call=True,
+    #)
+    #def download_file(n_clicks,data):
+    #    #return dcc.send_data_frame(df.to_csv, "mydf.csv")
+    #    return None
+    #
+    @callback(
+        Output(ids.datatable(MATCH), "data"),
+        Input(ids.addrow(MATCH), "n_clicks"),
+        State(ids.datatable(MATCH), "data"),
+        State(ids.datatable(MATCH), "columns"),
+    )
+    def add_row(n_clicks, data, columns):
+        if n_clicks > 0:
+            data.append({c["id"]: "" for c in columns})
+        return data
+
+    # @callback()
+    # def table_update():
+    #    # maybe this should be moved to some other place?
+    #    # this is more focused on how this component changes other visual outside of the data table
+    #    pass
+
+
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    if 'csv' in filename:
+        # Assume that the user uploaded a CSV file
+        return pd.read_csv(
+            io.StringIO(decoded.decode('utf-8')))
+    elif 'xls' in filename:
+        # Assume that the user uploaded an excel file
+        return pd.read_excel(io.BytesIO(decoded))
